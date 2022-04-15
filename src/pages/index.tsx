e230369c88f238118c6451985c7fb5b0a7cbc868
vittis <vitbolb1@gmail.com>
2022-04-15 18:50:46 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1 className="teste text-4xl font-bold underline">Hello world!</h1>
+      <div className="flex">
+        <div>oi</div>
+        <div>qq</div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
